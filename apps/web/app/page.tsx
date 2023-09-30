6f28e8e0b453d3b5b2675ac4eebbe13f24842ae4
irvile <irvile.rodrigues@gmail.com>
2023-09-30 17:50:31 -0300

    feat(web): add backend api

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,16 @@
 import Image from 'next/image'
+import { api } from 'backend-api'
 
-export default function Home() {
+async function getData() {
+	const response = await api['hello-world'].get()
+	return response
+}
+
+export default async function Home() {
+	const response = await getData()
+
+	console.log('Response from backend', response.data)
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
